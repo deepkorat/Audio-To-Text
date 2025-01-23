4b8fb7cd2005a04c0ed25c3885928b8d3de3607a
Deep Korat <deepkorat13@gmail.com>
2025-01-23 11:09:00 +0530

    changes in server file

--- example.py
+++ example.py
@@ -10,7 +10,7 @@ class MyServer(BaseHTTPRequestHandler):
           self.send_response(200)
           self.send_header("Content-type", "text/html")
           self.end_headers()
-          self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+          self.wfile.write(bytes("<html><head><title>Deep Korat</title></head>", "utf-8"))
           self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
           self.wfile.write(bytes("<body>", "utf-8"))
           self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+        print("\nServer is shutting down...")
+        webServer.server_close()  # Close the server
+        print("Server stopped.")
+        sys.exit(0)  #
